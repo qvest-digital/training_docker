b8a94b59f4b6bb96b33c9a06381489ddb6c45c50
Volker Schmitz <v.schmitz@tarent.de>
2019-02-21 17:14:45 +0100

    docker-compose updates

--- examples/wordpress/wordpress.sh
+++ examples/wordpress/wordpress.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-if [ "$1" == "-d" ]; then
-  ARGS="-d"
-else
-  ARGS=""
-fi
-
-docker run $ARGS --link=wordpress-database -p 8080:80 wordpress
+docker run -d --link=wordpress-database -p 8080:80 wordpress
