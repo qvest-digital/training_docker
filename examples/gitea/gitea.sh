adb1c6ea2abb0295e6c5d51131e53972392c021d
Volker Schmitz <v.schmitz@tarent.de>
2019-02-21 14:41:06 +0100

    fix gitea.sh

--- examples/gitea/gitea.sh
+++ examples/gitea/gitea.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --link gitea-database -p 3000:3000 -p 22:3022 -v /root/examples/gitea/data:/data gitea/gitea
+docker run -d --link gitea-database -p 3000:3000 -p 22:3022 -v /root/examples/gitea/data:/data gitea/gitea
